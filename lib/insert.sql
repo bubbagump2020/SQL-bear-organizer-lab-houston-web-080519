291f0ae564703d0f0eab85ee0fbd0f0711225cb5
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-15 13:34:17 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
-
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "male", "brown", "sanguine", true)
